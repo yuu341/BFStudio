e931b407dd30b7205e3a1405c1e1a9c99dfc03d0
yusuke furuya <oz.g.341@gmail.com>
2015-04-20 02:35:29 +0900

    基礎作成

---
+++ BFStudio/Pages/UserSettings/Controllers/UserSettingsController.cs
@@ -0,0 +1,32 @@
+﻿using BFStudio.Pages.UserSettings.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+//using BFStudio.Entity;
+
+namespace BFStudio.Pages.UserSettings.Controllers
+{
+    public class UserSettingsController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            UserSettingsModel model = new UserSettingsModel();
+
+            return View(model);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BFStudio/Pages/UserSettings/Models/UserSettingsModel.cs
@@ -0,0 +1,13 @@
+﻿using BFStudio.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BFStudio.Pages.UserSettings.Models
+{
+    public class UserSettingsModel
+    {
+        DBManageEntities db = new DBManageEntities();
+    }
+}
